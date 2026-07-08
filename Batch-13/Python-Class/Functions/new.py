fc9faf6af763fbfcbf5a2661f31d04df6a81dbd4
israel-oye <ioyeboade@gmail.com>
2026-07-08 13:46:55 +0100

    add code files for functions

---
+++ Batch-13/Python-Class/Functions/new.py
@@ -0,0 +1,19 @@
+def square(number: int):
+    return number ** 2
+
+print(square(12))
+print(square(5))
+
+# Create a func: celsius_to_fahrenheit(temp)
+# F = 1.8 x C + 32
+
+def celsius_to_fahrenheit(temp: float):
+    f = 1.8 * temp + 32
+    return f
+    print("After return")
+
+celsius_to_fahrenheit(37)
+
+
+if False:
+    print("")
