faebfe3efa5852b396d03809a782ff13efa5c6a4
israel-oye <ioyeboade@gmail.com>
2026-08-04 16:42:13 +0100

    add updated files

--- Batch-13/Python-Class/Serialization/main.dart
+++ Batch-13/Python-Class/Serialization/main.dart
@@ -4,4 +4,6 @@ void main(){
   String jsonStr = '{"classname": "Dev with Python", "students": 20, "is_completed": false, "reg_no": null}';
   var map = jsonDecode(jsonStr);
   print(map['is_completed']);
+
+  // if map[]
 }
