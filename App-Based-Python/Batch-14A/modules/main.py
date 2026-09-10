8dce6282261a9a0da0b340fd440bfd40b69fc706
israel-oye <ioyeboade@gmail.com>
2026-09-10 14:09:29 +0100

    Add code files on recursion, lambda, HOF, Modules

---
+++ App-Based-Python/Batch-14A/modules/main.py
@@ -0,0 +1,7 @@
+from operators import *
+
+# print(add(3, 5))
+
+multiply(3, 3)
+
+print(__name__)
---
+++ App-Based-Python/Batch-14A/modules/operators.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+if __name__ == '__main__':
+    print(__name__)
